b36399449ddffd6e2049f4057825e845470d122b
Skander Blaiti <skander.blaiti@gmail.com>
2025-07-06 21:09:28 +0100

    chore(scope): add tests

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,6 @@
+import { fontScale, verticalScale, isSmallDevice, isTablet } from '../index';
+
+expect(fontScale).toBeDefined();
+expect(verticalScale).toBeDefined();
+expect(isSmallDevice).toBeDefined();
+expect(isTablet).toBeDefined();
