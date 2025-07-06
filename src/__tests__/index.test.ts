7ec950885e2c561f481433bafdbdaec3b00ea6df
Skander Blaiti <skander.blaiti@gmail.com>
2025-07-06 21:13:07 +0100

    chore(scope): Fix tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,6 +1,19 @@
 import { fontScale, verticalScale, isSmallDevice, isTablet } from '../index';
 
-expect(fontScale).toBeDefined();
-expect(verticalScale).toBeDefined();
-expect(isSmallDevice).toBeDefined();
-expect(isTablet).toBeDefined();
+describe('react-native-mixins', () => {
+  test('should export fontScale', () => {
+    expect(fontScale).toBeDefined();
+  });
+
+  test('should export verticalScale', () => {
+    expect(verticalScale).toBeDefined();
+  });
+
+  test('should export isSmallDevice', () => {
+    expect(isSmallDevice).toBeDefined();
+  });
+
+  test('should export isTablet', () => {
+    expect(isTablet).toBeDefined();
+  });
+});
